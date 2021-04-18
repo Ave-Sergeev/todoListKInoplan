9a7dcf5030e083503625c17ea42fc2fa7fb77efe
artem <twin7366@mail.ru>
2021-04-18 21:36:24 +0300

    dopil#3, fixed actors.

--- app/actions/TodoAction.scala
+++ app/actions/TodoAction.scala
@@ -1,11 +1,13 @@
 package actions
 
+import models.Task
 import play.api.libs.json.Json
-import play.api.mvc.Results.NotFound
+import play.api.mvc.Results.{BadRequest, NotFound}
+
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 import play.api.mvc._
-import reactivemongo.api.bson.BSONObjectID
+import reactivemongo.api.bson.{BSONDocument, BSONObjectID}
 import services.TaskService
 
 @Singleton
@@ -16,16 +18,27 @@ class TodoAction @Inject()(
   taskService: TaskService
 ) {
 
-  def todoAction(id: BSONObjectID): ActionBuilder[TodoRequest, AnyContent] =
+  def todoAction(id: BSONObjectID): ActionBuilder[TodoRequest, AnyContent] = {
+
     new ActionBuilder[TodoRequest, AnyContent] {
+      
       def executionContext: ExecutionContext = ec
+
       override def parser: BodyParser[AnyContent] = defaultParser
+
       override def invokeBlock[A](request: Request[A], block: TodoRequest[A] =>
         Future[Result]): Future[Result] = {
-        taskService.findOne(id).map(taskO =>
-          taskO.map(new TodoRequest(_, request))
-            .toRight(NotFound(Json.obj("error" -> "placement not found")))
-        )
+
+        request match {
+          case req: TodoRequest[A] => block(req)
+          case _ => Future.successful(BadRequest("400 Invalid Request"))
+        }
       }
     }
+  }
 }
+
+//taskService.findOne(id).map(taskO =>
+//taskO.map(new TodoRequest(_, request))
+//.toRight(NotFound(Json.obj("error" -> "placement not found")))
+//)
--- app/actions/TodoActionT.scala
+++
@@ -1,6 +0,0 @@
-package actions
-
-import play.api.mvc.{ActionBuilder, ActionRefiner, AnyContent, Request}
-
-trait TodoActionT extends ActionBuilder[TodoRequest, AnyContent]
-  with ActionRefiner[Request, TodoRequest]
